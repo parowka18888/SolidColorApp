1d6de46d8078cbda40fa6fbce684d1aec97344ed
flapdzak <parowka18888@gmail.com>
2026-03-22 13:53:26 +0100

    Integrated color change functionality

--- lib/features/color_controller/color_controller.dart
+++ lib/features/color_controller/color_controller.dart
@@ -9,6 +9,14 @@ class ColorController{
   static const double _opacity = 1.0;
   static const int _maxColors = 255;
 
+  static Color _fontColor = getRandomColor();
+  static Color _backgroundColor = getRandomColor();
+
+  /// getter for font color
+  static Color get fontColor => _fontColor;
+  /// getter for background color
+  static Color get backgroundColor => _backgroundColor;
+
   /// returns random RGB color
   static Color getRandomColor(){
     return Color.fromRGBO(
@@ -17,6 +25,12 @@ class ColorController{
         _random.nextInt(_maxColors),
         _opacity
     );
+  }
+
+  /// sets new colors for variables
+  static void setColors(){
+    _fontColor = getRandomColor();
+    _backgroundColor = getRandomColor();
   }
 
 }
--- lib/presentation/hello_screen/hello_screen.dart
+++ lib/presentation/hello_screen/hello_screen.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:solidcolorapp/features/color_controller/color_controller.dart';
 
+/// class for defining hello screen
 class HelloScreen extends StatefulWidget{
+  /// class constructor
   const HelloScreen({super.key});
   @override
-  State<HelloScreen> createState() => _HelloScreen();
+  State<HelloScreen> createState() => _HelloScreenState();
 }
+/// class for hello screen state
+class _HelloScreenState extends State<HelloScreen> {
 
-class _HelloScreen extends State<HelloScreen> {
+  ColorController colorController = ColorController();
 
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-        backgroundColor: ColorController.getRandomColor(),
-        body: Center(
-          child: Text(
-            "Hello  there!",
-            style: TextStyle(
-                color: ColorController.getRandomColor(),
-                fontSize: 100,
-                fontWeight: FontWeight.bold
+    return GestureDetector(
+      onTap: () => setState(ColorController.setColors),
+      child: Scaffold(
+          backgroundColor: ColorController.backgroundColor,
+          body: Center(
+            child: Text(
+              "Hello  there!",
+              style: TextStyle(
+                  color: ColorController.fontColor,
+                  fontSize: 100,
+                  fontWeight: FontWeight.bold
+              ),
             ),
-          ),
-        )
+          )
+      ),
     );
   }
 }
